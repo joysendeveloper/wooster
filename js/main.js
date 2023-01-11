da548584669f86574e4f54d66fbd296bdb06dae3
Joy Sen <119527966+joysendeveloper@users.noreply.github.com>
2023-01-11 12:13:35 +0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function opennav(){
+  document.getElementById("content").style.width="400px";
+}
+function closenav(){
+  document.getElementById("content").style.width="0px";
+}
+$(document).ready(function(){
+  //mixitup portfolio section
+  var mixer = mixitup('.joy');
+});
